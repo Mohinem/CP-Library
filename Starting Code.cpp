51f15ccfd57f1f5c63c0ba4d5ea900b4c01c6675
Mohinem <rickyigl10@gmail.com>
2020-04-02 16:26:53 +0530

    Added exponentiation mistake
    Added exponentiation mistake

--- Starting Code.cpp
+++ Starting Code.cpp
@@ -19,5 +19,6 @@ Relevant problem where I made mistake - https://codeforces.com/contest/1326/prob
 2. Try using constant value of N to declare arrays, etc. Else you might get hacked / FST.
 Relevant problem where I made a mistake - https://codeforces.com/contest/1307/problem/D
 
-
+3. While exponentiation, divide the power by mod-1 of phi(mod) for large value, because the exponent will repeat according to Fermat's Little Theorem / Euler's Theorem.
+Relevant problem where I made a mistake - https://codeforces.com/contest/1332/problem/E
 */
